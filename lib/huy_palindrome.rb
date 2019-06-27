2ccbb489d4899d0ec95a1dffb0b5e2c104fb562e
QuocHuy <huynikee37@gmail.com>
2019-06-27 20:23:46 +0700

    Initialize repository

---
+++ lib/huy_palindrome.rb
@@ -0,0 +1,6 @@
+require "huy_palindrome/version"
+
+module HuyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/huy_palindrome/version.rb
@@ -0,0 +1,3 @@
+module HuyPalindrome
+  VERSION = "0.1.0"
+end
